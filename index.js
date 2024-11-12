cc21a35d3ad09c4f5e311bb3a7ece2ade04d41dd
Semyon1101o <146949498+Semyon1101o@users.noreply.github.com>
2024-11-12 13:53:09 +0000

    done

--- index.js
+++ index.js
@@ -1,57 +1,38 @@
+import PostalValidator from './src/PostalCodeSchema.js';
+import IpValidator from './src/IpAddressSchema.js';
+import UserValidator from './src/UserSchema.js';
+
 class Validator {
-    constructor(){
-        // this.options = {...props};
-    };
-    postalCode() {
-        this.startsWith = 'ZIP';
-        return this;
-    }
+  postalCode() {
+    return new PostalValidator();
+  }
 
-    setPostalCodeLengthConstraint(mn, mx = Infinity) {
-        this.minLength = mn;
-        this.maxLength = mx;
-        return this;
-    };
+  ipAddress() {
+    return new IpValidator();
+  }
 
-    isCode(str) {
-        return `${str}`.startsWith(this.startsWith);
-    };
-
-    isLengthValid(str) {
-        if (this.minLength === undefined) {
-            return true;
-        }
-        return `${str}`.split('_').at(1).length >= this.minLength &&
-        `${str}`.split('_').at(1).length <= this.maxLength;
-    }
-
-    isValid(str) {
-        return this.isCode(str) && this.isLengthValid(str);
-    }
-};
+  user() {
+    return new UserValidator();
+  }
+}
 
 // const v = new Validator();
-
 // const postalCodeSchema = v.postalCode();
+// console.log(postalCodeSchema);
 
 // console.log(postalCodeSchema.isValid('ZIP_12345')); // true
 // postalCodeSchema.isValid('ZIP_67890'); // true
 // postalCodeSchema.isValid('ZIP_54321'); // true
-// console.log(postalCodeSchema.isValid('invalid-postal-code')); // false
-// postalCodeSchema.isValid(1234567890); // false
+// postalCodeSchema.isValid('invalid-postal-code'); // false
+// console.log(postalCodeSchema.isValid(1234567890)); // false
 
 const v = new Validator();
 
-const postalCodeSchema1 = v.postalCode();
-postalCodeSchema1.isValid('ZIP_12345'); // true
+// Позволяет описывать валидацию для свойств пользователя
+const userSchema = v.user().shape({
+  postalCode: v.postalCode().setPostalCodeLengthConstraint(7),
+  ipAddress: v.ipAddress().ipAddressValues(4, 6),
+});
 
-const postalCodeSchema2 = v.postalCode().setPostalCodeLengthConstraint(7);
-console.log(postalCodeSchema2);
-console.log(postalCodeSchema2.isValid('ZIP_6789056889')); // true
-console.log(postalCodeSchema2.isValid('ZIP_54321')); // false
-
-const postalCodeSchema3 = v.postalCode().setPostalCodeLengthConstraint(4, 6);
-console.log(postalCodeSchema3.isValid('ZIP_54321')); // true
-postalCodeSchema3.isValid('ZIP_67'); // false
+console.log(userSchema);
 export default Validator;
-
